9e5ea3db1c59a68c0c2b7fa2460636d8d824bf9d
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 21:20:30 +0000

    Adding parser class

---
+++ src/com/developech/behaviorsClasses/CvParser.java
@@ -0,0 +1,34 @@
+package com.developech.behaviorsClasses;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class CvParser extends DefaultHandler {
+	
+	@Override
+	public void startDocument() throws SAXException{
+		
+	}
+	
+	@Override
+	public void startElement(String uri,String localName,String qName,Attributes attributes) throws SAXException{
+		
+	}
+	
+	@Override
+	public void characters(char[] charachters,int start,int length) throws SAXException{
+		
+	}
+	
+	@Override
+	public void endElement(String uri,String localName,String qName) throws SAXException{
+		
+	}
+	
+	@Override
+	public void endDocument() throws SAXException{
+		
+	}
+	
+}
