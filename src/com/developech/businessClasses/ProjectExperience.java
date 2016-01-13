1889495cf4c96bf598c1bfe337e299e3eaae76ae
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 18:59:57 +0000

    Creating and adding code to CLass {ProjectExperience}

---
+++ src/com/developech/businessClasses/ProjectExperience.java
@@ -0,0 +1,9 @@
+package com.developech.businessClasses;
+
+public class ProjectExperience extends Experience {
+
+	public ProjectExperience(String title, String startDate, String endDate) {
+		super(title, startDate, endDate);
+	}
+
+}
