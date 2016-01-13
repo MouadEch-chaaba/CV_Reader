553ae0de8cecb4a68e3a58500284be7e2c3f2717
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 17:46:57 +0000

    Adding code to ELevel Enum

--- src/com/developech/businessClasses/ELevel.java
+++ src/com/developech/businessClasses/ELevel.java
@@ -1,5 +1,19 @@
 package com.developech.businessClasses;
 
 public enum ELevel {
-
+	MOTHER("mother"),
+	EXCELLENT("excellent"),
+	INTERMIDIATE("intermidiate"),
+	MINIMAL("minimal");
+	
+	private String level;
+	
+	ELevel(String level){
+		this.level = level;
+	}
+	
+	@Override
+	public String toString(){
+		return this.level;
+	}
 }
