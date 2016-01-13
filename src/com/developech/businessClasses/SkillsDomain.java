5b657ff001f7e45c07d1f900666016000b170593
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 20:20:54 +0000

    Creating and adding code to Class {SkillsDomain}

---
+++ src/com/developech/businessClasses/SkillsDomain.java
@@ -0,0 +1,31 @@
+package com.developech.businessClasses;
+
+import java.util.ArrayList;
+
+public class SkillsDomain {
+	private String domainName;
+	private ArrayList<SkillsDomainItem> items; 
+	
+	public SkillsDomain(){}
+	public SkillsDomain(String domainName,ArrayList<SkillsDomainItem> items){
+		this.domainName = domainName;
+		this.items = items;
+	}
+	
+	/* Getters And Setters */
+	public String getDomainName(){
+		return this.domainName;
+	}
+	
+	public ArrayList<SkillsDomainItem> getItems(){
+		return this.items;
+	}
+	
+	public void setDomainName(String domainName){
+		this.domainName = domainName;
+	}
+	
+	public void setItems(ArrayList<SkillsDomainItem> items){
+		this.items = items;
+	}
+}
