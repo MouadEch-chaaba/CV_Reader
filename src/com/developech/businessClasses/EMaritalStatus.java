8aedbb3f5500ebb4d0bccb3f34babfedff746e2c
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 17:49:00 +0000

    Adding code to EMaritalStatus Enum

--- src/com/developech/businessClasses/EMaritalStatus.java
+++ src/com/developech/businessClasses/EMaritalStatus.java
@@ -1,5 +1,18 @@
 package com.developech.businessClasses;
 
 public enum EMaritalStatus {
-
+	MARIED("maried"),
+	SINGLE("single"),
+	DIVORCED("divorced");
+	
+	private String maritalStatus;
+	
+	EMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	
+	@Override
+	public String toString(){
+		return this.maritalStatus;
+	}
 }
