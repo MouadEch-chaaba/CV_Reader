a467223fc97ecb11858b2f96562b47a3c87c0db3
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-13 17:42:34 +0000

    Adding code to ESexe Enum

--- src/com/developech/businessClasses/ESexe.java
+++ src/com/developech/businessClasses/ESexe.java
@@ -1,5 +1,17 @@
 package com.developech.businessClasses;
 
 public enum ESexe {
-
+	MALE("male"),
+	FEMALE("female");
+	
+	private String sexe;
+	
+	ESexe(String sexe){
+		this.sexe = sexe;
+	}
+	
+	@Override
+	public String toString(){
+		return this.sexe;
+	}
 }
