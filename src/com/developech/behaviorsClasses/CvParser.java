dc310a533bc4552a8745f23ce02e5c31b15fdb7f
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-01-15 11:05:25 +0000

    Adding the addItem method to SkillsDomain class and updating the default constructor

--- src/com/developech/behaviorsClasses/CvParser.java
+++ src/com/developech/behaviorsClasses/CvParser.java
@@ -1,10 +1,31 @@
 package com.developech.behaviorsClasses;
 
+import java.util.ArrayList;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import com.developech.businessClasses.Certificate;
+import com.developech.businessClasses.Cv;
+import com.developech.businessClasses.Education;
+import com.developech.businessClasses.Experience;
+import com.developech.businessClasses.Language;
+import com.developech.businessClasses.OtherInfosItem;
+import com.developech.businessClasses.Person;
+import com.developech.businessClasses.SkillsDomain;
+import com.developech.businessClasses.SkillsDomainItem;
+
 public class CvParser extends DefaultHandler {
+	private Cv cv;
+	private Person owner;
+	private ArrayList<Experience> experiences;
+	private ArrayList<Language> languages;
+	private ArrayList<SkillsDomain> skills;
+	private ArrayList<SkillsDomainItem> currentSkillsDomainItems;
+	private ArrayList<Education> educations;
+	private ArrayList<Certificate> certifications;
+	private ArrayList<OtherInfosItem> otherInformations;
 	
 	@Override
 	public void startDocument() throws SAXException{
